a8207a0314980b579dab2189a8c3f0286dd439bd
larissalages <larissalages7@gmail.com>
2020-07-23 18:52:59 -0300

    binary search

--- leetcode/search_insert_position.py
+++ leetcode/search_insert_position.py
@@ -29,5 +29,21 @@ class Solution:
                 i+=1
         return i
 
+# Binary Search
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        
+        while(l<=r):
+            mid = (l+r)/2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] <= target:
+                l = mid+1
+            else:
+                r = mid-1
+        return l
+
 index = Solution().searchInsert([1,3,5,6], 5)
 print(index)
