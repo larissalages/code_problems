cb328f99a51611771be80c84bf37802c4ed964e5
larissalages <larissalages7@gmail.com>
2020-08-05 20:17:16 -0300

    refactore balance binary treee

--- leetcode/balance_binary_search_tree.py
+++ leetcode/balance_binary_search_tree.py
@@ -5,49 +5,61 @@
 # If there is more than one answer, return any of them.
 
 # Definition for a binary tree node.
- class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+class TreeNode:
+	def __init__(self, val=0, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
 
 	def insert(self, data):
-	    if self.data:
-	        if data < self.data:
-	            if self.left is None:
-	                self.left = Node(data)
-	                return
-	            else:
-	                self.left.insert(data)
-	        elif data > self.data:
-	            if self.right is None:
-	                self.right = Node(data)
-	                return
-	            else:
-	                self.right.insert(data)
-	    else:
-	        self.data = data
+		if self.val:
+			if data < self.val:
+				if self.left is None:
+					self.left = Node(data)
+					return
+				else:
+					self.left.insert(data)
+			elif data > self.data:
+				if self.right is None:
+					self.right = Node(data)
+					return
+				else:
+					self.right.insert(data)
+		else:
+			self.val = data
 
 def tree2list(sorted_list, node):
 	if node.left:
-		tree2list(sorted_list, node.left)
+		sorted_list.append(tree2list(sorted_list, node.left))
 	sorted_list.append(node)
 	if node.right:
-		tree2list(sorted_list, node.right)
+		sorted_list.append(tree2list(sorted_list, node.right))
 	if node.left is None or node.right is None:
-		return sorted_list
+		return node.val
 
 def insert_balance_tree(sorted_list, tree):
-	mid = len(sorted_list)/2
-	tree.insert(sorted_list[insert])
-	insert_balance_tree(sorted_list[0:mid], tree)
-	insert_balance_tree(sorted_list[mid+1:len(sorted_list)], tree)
-
+	if not sorted_list:
+		return 
+	if isinstance(sorted_list, list): 
+		mid = len(sorted_list)//2
+		tree.insert(sorted_list[mid])
+		insert_balance_tree(sorted_list[0:mid], tree)
+		insert_balance_tree(sorted_list[mid+1:len(sorted_list)], tree)
+	else: 
+		mid = sorted_list
+		tree.insert(mid)
+		return 
 
 class Solution:
-    def balanceBST(self, root: TreeNode) -> TreeNode:
-    # TODO: call tree2list and insert_balance_tree
+	def balanceBST(self, root: TreeNode) -> TreeNode:
+		list_tree = []
+		list_tree = tree2list(list_tree, root)
+		new_root = TreeNode()
+		new_root.val = None
+		insert_balance_tree(list_tree, new_root)
+		return new_root
+		
 
 
-            
-        
+			
+		
