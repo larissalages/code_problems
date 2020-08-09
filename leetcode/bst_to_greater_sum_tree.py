6a2b3fe0b5024b6f28413255cd101e76eb933b77
larissalages <larissalages7@gmail.com>
2020-08-09 12:20:30 -0300

    cleaning code

--- leetcode/bst_to_greater_sum_tree.py
+++ leetcode/bst_to_greater_sum_tree.py
@@ -6,20 +6,21 @@
 #         self.right = right
 class Solution:
 	def bstToGst(self, root: TreeNode) -> TreeNode:
+
 		def sum_tree(node, sum_aux):
 			if not node:
-				return 0, node;
+				return 0
 			if node.right:
-				sum_aux, _ = sum_tree(node.right, sum_aux)
+				sum_aux = sum_tree(node.right, sum_aux)
 
 			sum_aux = sum_aux + node.val
 			node.val = sum_aux
 
 			if node.left:
-				sum_aux, _ = sum_tree(node.left, sum_aux)
+				sum_aux = sum_tree(node.left, sum_aux)
 
-			return sum_aux, node
+			return sum_aux
 
-		_, new_tree = sum_tree(root, 0)
-		return new_tree
+		sum_tree(root, 0)
+		return root
 
