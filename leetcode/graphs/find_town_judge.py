879adf3176202c8686a88d197adeeb86ec2e4255
larissalages <larissalages7@gmail.com>
2020-08-15 15:07:20 -0300

    a muche clear solution for find the town judge

--- leetcode/graphs/find_town_judge.py
+++ leetcode/graphs/find_town_judge.py
@@ -1,5 +1,17 @@
-class Solution:
+# In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+
+# If the town judge exists, then:
+
+# The town judge trusts nobody.
+# Everybody (except for the town judge) trusts the town judge.
+# There is exactly one person that satisfies properties 1 and 2.
+# You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+
+# If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+
+class Solution1:
 	def findJudge(self, N, trust):
+		if N ==1: return N
 		graph = {}
 		for pair in trust:
 			p1 = pair[0]
@@ -23,6 +35,18 @@ class Solution:
 
 		return poss_judge
 
+# A MUCH better approach
+class Solution2:
+	def findJudge(self, N, trust):
+		count = [0] * (N+1)
+		for i, j in trust:
+			count[i] -= 1
+			count[j] += 1
+		for i in range(1, N+1):
+			if count[i] == N-1:
+				return i
+		return -1
 
-s = Solution()
+
+s = Solution1()
 print(s.findJudge(2,[[1,2]]))
