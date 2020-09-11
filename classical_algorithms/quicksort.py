c3497bc53ad691da46554350335c27e1cf1c8c31
larissalages <larissalages7@gmail.com>
2020-09-10 21:06:06 -0300

    add quicksort

---
+++ classical_algorithms/quicksort.py
@@ -0,0 +1,25 @@
+class Quicksort:
+    def sort(self, data):
+        if data is None:
+            raise TypeError('data cannot be None')
+        return self._sort(data)
+
+    def _sort(self, data):
+        if len(data) < 2:
+            return data
+        equal, left, right = []
+        piv_index = len(data)//2
+        piv_value = data[piv_index]
+
+        for n in data:
+            if n == piv_value:
+                equal.append(n)
+            if n < piv_value:
+                left.append(n)
+            else:
+                right.append(n)
+
+        left = self._sort(left)
+        right = self._sort(right)
+
+        return left + equal + right
