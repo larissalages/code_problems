3aa4f3fd6e504e936f1d5cf771c94c17f3dffc66
avk29 <66971874+avk29@users.noreply.github.com>
2020-10-05 02:04:30 +0530

    Update Bubble Sort.py

--- classical_algorithms/python/Bubble Sort.py
+++ classical_algorithms/python/Bubble Sort.py
@@ -3,13 +3,10 @@ def bubblesort(array):
     n = len(array)
 
     for i in range(0,n-1):
-
         for j in range(0,n-i-1):
-
             if (array[j] > array[j+1]):
-
                 array[j] , array[j+1] = array[j+1] , array[j]
-
+                
     print(array)
 
 
