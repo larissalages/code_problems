42f786d4c5418ed9682b2235460cb8819ef8770c
MDarshan123 <72216152+MDarshan123@users.noreply.github.com>
2020-10-04 22:19:01 +0530

    added pattern problems

---
+++ Patterns/Pyramid/Full pyramid.py
@@ -0,0 +1,12 @@
+def full(n): 
+	k = 2*n - 2
+
+	for i in range(0, n): 
+		for j in range(0, k): 
+			print(end=" ") 
+		k = k - 1
+		for j in range(0, i+1): 
+			print("* ", end="") 
+		print("\r") 
+n = 5
+full(n) 
