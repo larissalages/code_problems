2fa11ef81b12f877a09b1a30ed5848271b35452c
bamblebam <burhanizrangwala@gmail.com>
2020-10-08 11:52:37 +0530

    added q1 special array

---
+++ leetcode/weekly contests/week 209/Q1_special_array.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)+1):
+            count = 0
+            for j in nums:
+                if j >= i:
+                    count += 1
+            if count == i:
+                return i
+        return -1
