800f6c4865d03e93ffc51bfd5326e075cda4ec32
bamblebam <burhanizrangwala@gmail.com>
2020-10-08 14:02:14 +0530

    crawler log problem

--- leetcode/weekly contest problems/special_array_with_X_elements_greater_than_X.py
+++ leetcode/weekly contest problems/special_array_with_X_elements_greater_than_X.py
@@ -1,4 +1,9 @@
 from typing import List
+# You are given an array nums of non-negative integers. nums is considered special if there exists a number x such that there are exactly x numbers in nums that are greater than or equal to x.
+
+# Notice that x does not have to be an element in nums.
+
+# Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
 
 
 class Solution:
