ef5ebb5f6a06c8530909fec2c28edcb61b1dff4b
ManishShah120 <mkshah141@gmail.com>
2020-10-15 13:36:22 +0530

    Added a Pattern Problem

---
+++ Patterns/Ladderophilia/Ladderophilia.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+    {
+        printf("\n*   *");
+        printf("\n*   *");
+        printf("\n*****");
+    }
+    printf("\n*   *");
+    printf("\n*   *");
+    return 0;
+}
