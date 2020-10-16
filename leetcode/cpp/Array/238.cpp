f2cb0ee0e61edc79421ef3305477ccfceeb3939b
Shruthi Rao <55725103+shruthi019@users.noreply.github.com>
2020-10-16 19:05:06 +0530

    Added cpp solution for 238

---
+++ leetcode/cpp/Array/238.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = nums[i - 1] * ans[i - 1];
+        }
+        
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * right;
+            right *= nums[i];
+        }
+        
+        return ans;
+    }
+};
