9f3c7d670c23021a4074c1704f321b215c54d57b
simran2607 <56145342+simran2607@users.noreply.github.com>
2020-10-21 16:04:07 +0530

    added leetcode problems #27

--- leetcode/cpp/Two Pointer/75.cpp
+++ leetcode/cpp/Two Pointer/75.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int  low=0,mid=0,high=nums.size()-1;
-        while(mid<=high){
-            
-            if(nums[mid]==0)
-            {
-              swap(nums[low],nums[mid]);
-                low++,mid++;
-            }
-            else if(nums[mid]==1)
-            {
-                mid++;
-            }
-            else{
-                swap(nums[mid],nums[high]);
-                high--;
-            }
-        }
+    void sortColors(vector<int>& A) {
+        int count_0=0,count_1=0,count_2=0;
+    for(int i=0;i<A.size();i++)
+    {
+        if(A[i]==0)
+         count_0++;
+        else if(A[i]==1)
+         count_1++;
+        else if(A[i]==2)
+         count_2++;
+    }
+    for(int i=0;i<count_0;i++)
+      A[i]=0;
+    for(int i=count_0;i<count_0+count_1;i++)
+      A[i]=1;
+    for(int i=count_1+count_0;i<count_0+count_1+count_2;i++)
+      A[i]=2;
     }
 };
