3da993c5ba0c52f054c8b50f4709e022a331e9cb
Saideepthi <49442816+Deepthi2001@users.noreply.github.com>
2020-10-22 17:51:59 +0530

    Update 56.cpp

--- leetcode/cpp/Array/56.cpp
+++ leetcode/cpp/Array/56.cpp
@@ -4,7 +4,6 @@ public:
         vector<vector<int>>res;
         int n=v.size();
         if(n<=1) return v;
-        //vector<pair<int,int>>v(intervals.begin(),intervals.end());
         sort(v.begin(),v.end());
         for(int i=0;i<n-1;i++){
             if(v[i][1]>=v[i+1][0]){
