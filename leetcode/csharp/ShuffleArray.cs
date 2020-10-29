40d7bc06824fb6b505af4bc9c879d394bd5f2ad1
SamudraThio <68200757+SamudraThio@users.noreply.github.com>
2020-10-28 19:48:49 -0500

    Update ShuffleArray.cs

--- leetcode/csharp/ShuffleArray.cs
+++ leetcode/csharp/ShuffleArray.cs
@@ -1,58 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LeetCode_Master
+﻿public class ShuffleArray
 {
-    public class ShuffleArray
+    int[] arrayOriginal;
+    int[] arrayCopy;
+
+    public ShuffleArray(int[] nums)
     {
-        int[] arrayOriginal;
-        int[] arrayCopy;
-
-        public ShuffleArray(int[] nums)
-        {
-            arrayOriginal = (int[])nums.Clone();
-            arrayCopy = nums;
-        }
-
-        public int[] Reset()
-        {
-            return arrayOriginal;
-        }
-        public int[] Shuffle()
-        {
-            Random random = new Random();
-
-            for (int i = 0; i < arrayCopy.Length; i++)
-            {
-               swapPlaces(i, random.Next(0, arrayCopy.Length));
-            }
-            return arrayCopy;
-        }
-
-        private void swapPlaces(int i, int j)
-        {
-            int temp = arrayCopy[i];
-            arrayCopy[i] = arrayCopy[j];
-            arrayCopy[j] = temp;
-        }
+        arrayOriginal = (int[])nums.Clone();
+        arrayCopy = nums;
     }
 
-    /*
-    static void Main(string[] args)
+    public int[] Reset()
     {
-        int[] nums = { 1, 2, 3 };
-        ShuffleArray obj = new ShuffleArray(nums);
-        int[] param_1 = obj.Reset();
-        foreach (int param in param_1)
-        {
-            Console.WriteLine(param);
-        }
-        int[] param_2 = obj.Shuffle();
-        foreach (int param in param_2)
-        {
-            Console.WriteLine(param);
-        }
+        return arrayOriginal;
     }
-    */
+    public int[] Shuffle()
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < arrayCopy.Length; i++)
+        {
+            swapPlaces(i, random.Next(0, arrayCopy.Length));
+        }
+        return arrayCopy;
+    }
+
+    private void swapPlaces(int i, int j)
+    {
+        int temp = arrayCopy[i];
+        arrayCopy[i] = arrayCopy[j];
+        arrayCopy[j] = temp;
+    }
 }
