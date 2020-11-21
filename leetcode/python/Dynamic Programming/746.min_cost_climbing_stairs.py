25d88ff8fb9393a5dc3e349d0b9b833ab251e1d4
larissalages <larissalages7@gmail.com>
2020-11-21 15:35:39 -0300

    add optimezed solution for prob 746

--- leetcode/python/Dynamic Programming/746.min_cost_climbing_stairs.py
+++ leetcode/python/Dynamic Programming/746.min_cost_climbing_stairs.py
@@ -11,34 +11,42 @@
 # Output: 6
 # Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
 
+# # First and bad solution
+# class Solution:
+#     def __init__(self):
+#         self.mem_cost = []
+#
+#     def _init_mem_cost(self, x):
+#         self.mem_cost = [-1] * x
+#
+#     def calculateCost(self, i, cost):
+#         if i == 0 or i == 1:
+#             return cost[i]
+#         if self.mem_cost[i] != -1:
+#             return self.mem_cost[i]
+#         else:
+#             if i + 1 == len(cost):
+#                 c_1 = self.calculateCost(i - 1, cost)
+#                 c_2 = self.calculateCost(i - 2, cost)
+#                 if c_1 < c_2 + cost[i]:
+#                     return c_1
+#                 else:
+#                     return c_2 + cost[i]
+#             else:
+#                 self.mem_cost[i] = min(self.calculateCost(i - 1, cost), self.calculateCost(i - 2, cost)) + cost[i]
+#                 return self.mem_cost[i]
+#
+#     def minCostClimbingStairs(self, cost) -> int:
+#         self._init_mem_cost(len(cost))
+#         return self.calculateCost(len(cost)-1, cost)
 
-class Solution:
-    def __init__(self):
-        self.mem_cost = []
 
-    def _init_mem_cost(self, x):
-        self.mem_cost = [-1] * x
-
-    def calculateCost(self, i, cost):
-        if i == 0 or i == 1:
-            return cost[i]
-        if self.mem_cost[i] != -1:
-            return self.mem_cost[i]
-        else:
-            if i + 1 == len(cost):
-                c_1 = self.calculateCost(i - 1, cost)
-                c_2 = self.calculateCost(i - 2, cost)
-                if c_1 < c_2 + cost[i]:
-                    return c_1
-                else:
-                    return c_2 + cost[i]
-            else:
-                self.mem_cost[i] = min(self.calculateCost(i - 1, cost), self.calculateCost(i - 2, cost)) + cost[i]
-                return self.mem_cost[i]
-
+class Solution:  # Good solution
     def minCostClimbingStairs(self, cost) -> int:
-        self._init_mem_cost(len(cost))
-        return self.calculateCost(len(cost)-1, cost)
+        len_ = len(cost)
+        for i in range(2, len_):
+            cost[i] += min(cost[i-1], cost[i-2])
+        return min(cost[len_-1], cost[len_-2])
 
 
 cost = [0, 0, 1, 1]
