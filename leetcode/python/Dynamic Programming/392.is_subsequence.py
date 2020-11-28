dba02c822ce8eba352d413cabfdd6a8784f2088a
larissalages <larissalages7@gmail.com>
2020-11-28 16:46:54 -0300

    add solution for leetcode prob 392

--- leetcode/python/Dynamic Programming/392.is_subsequence.py
+++ leetcode/python/Dynamic Programming/392.is_subsequence.py
@@ -1,21 +1,15 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        pc = 0
-        count = 0
+        i=0
         for j in range(0, len(t)):
-            for it in range(pc, len(s)):
-                if t[j] == s[it]:
-                    if it == pc or it == pc + 1:
-                        pc = it
-                        count += 1
-                    else:
-                        return False
-        if count == len(s):
+            if i < len(s) and s[i] == t[j]:
+                i += 1
+        if i == len(s):
             return True
         else:
             return False
 
-
 t = 'abcde'
-s = 'ace'
+s = "bb"
+
 print(Solution().isSubsequence(s, t))
