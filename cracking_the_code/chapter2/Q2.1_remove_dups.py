439a19ff990d5c02c9ab77ef0dd3c7c751b58dda
larissalages <larissalages7@gmail.com>
2021-03-20 15:12:35 -0300

    clean code

--- cracking_the_code/chapter2/Q2.1_remove_dups.py
+++ cracking_the_code/chapter2/Q2.1_remove_dups.py
@@ -48,19 +48,21 @@ def remove_dups2(l):
 
 class Test(unittest.TestCase):
     test_cases = (
-        (LinkedList([2, 2, 2, 2, 2]), LinkedList([2])),
-        (LinkedList([]), LinkedList([])),
-        (LinkedList([1, 2, 3, 4, 5, 3]), LinkedList([1, 2, 3, 4, 5])),
-        (LinkedList(['a', 1, 'a']), LinkedList(['a', 1])),
-        (LinkedList(['b', 'b', 'a']), LinkedList(['b', 'a'])),
-        (LinkedList([5, 6, 5, 6]), LinkedList([5, 6]))
+        ([2, 2, 2, 2, 2], [2]),
+        ([], []),
+        ([1, 2, 3, 4, 5, 3], [1, 2, 3, 4, 5]),
+        (['a', 1, 'a'], ['a', 1]),
+        (['b', 'b', 'a'], ['b', 'a']),
+        ([5, 6, 5, 6], [5, 6]),
+        ([1, 1, 1, 1, 1, 1], [1])
     )
 
     def test_remove_dup(self):
         for lst, expected in self.test_cases:
+            lst = LinkedList(lst)
+            expected = LinkedList(expected)
             copy_lst_value = copy.deepcopy(lst)
             remove_dups2(lst)
-            #logging.info("List: {}; Result: {}; Expected: {}".format(copy_lst_value, lst, expected))
             assert lst == expected
 
 
