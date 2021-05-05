e8acf1a15b0319574b0e3c6e54e720601ed36ee4
larissalages <larissalages7@gmail.com>
2021-05-04 23:13:26 -0300

    add draft class MultiStack

---
+++ cracking_the_code/chapter3/Q3.1-three-in-one.py
@@ -0,0 +1,52 @@
+import logging
+
+class StackInfo:
+    def __init__(self, start, capacity, array):
+        self.start = start
+        self.capacity = capacity
+        self.end = capacity - 1
+        self.array = array
+        self.size = 0
+        self.last_index = self.start
+
+    def is_withing_stack_capacity(self, index):
+        if index < 0 or index >= len(self.array):
+            return False
+        return self.start <= index < self.end
+
+    def last_capacity_index(self):
+        return self.end
+
+    def last_element_index(self):
+        return self.last_index
+
+    def is_full(self):
+        return self.size == self.capacity
+
+    def is_empty(self):
+        return self.size == 0
+
+
+class MultiStack:
+    def __init__(self, nstacks, stack_size):
+        self.array = [0]*(nstacks*stack_size)
+        self.info = []  # list of StackInfo
+        for i in range(nstacks):
+            self.info.append(StackInfo(i*stack_size, stack_size))
+
+    def all_stacks_are_full(self):
+        number_of_items = 0
+        for i in range(len(self.info)):
+            number_of_items += self.info[i].size
+        return number_of_items == len(self.array)
+
+    def push(self, stack_number, value):
+        stack = self.info[stack_number]
+        if self.all_stacks_are_full:
+            logging.error('Array is full!! Cannot push anything else')
+        if stack.is_full():
+            self.expand(stack_number)  # implement function
+        stack.size += 1
+        self.array[stack.last_element_index()] = value
+
+
