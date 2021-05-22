19ea947523e4fd0de2387009710f78313d4d8cc3
larissalages <larissalages7@gmail.com>
2021-05-22 19:56:06 -0300

    print stacks

--- cracking_the_code/chapter3/Q3.3-stack-of-plates.py
+++ cracking_the_code/chapter3/Q3.3-stack-of-plates.py
@@ -24,14 +24,44 @@ class SetOfStacks:
     def pop(self):
         last_stack = self.lst_stacks[-1]
         value = last_stack.pop()
+        self.last_stack_size -= 1
         if last_stack.isEmpty():
             self.lst_stacks.pop(-1)  # rm empty stack
             self.last_stack_size = self.capacity
         return value
 
+    def print_stacks(self):
+        print('Printing Stacks')
+        for stk in self.lst_stacks:
+            stk.print()
+            print('----------')
+        print('Done printing stacks')
+
 
 def main():
-    print("jasjdlaksd")
+    # insert a bunch of items
+    weird_stack = SetOfStacks(3)
+    weird_stack.push('a')
+    weird_stack.push('b')
+    weird_stack.push('c')
+    weird_stack.push('d')
+    weird_stack.push('e')
+    weird_stack.push('f')
+    weird_stack.push('g')
+    weird_stack.push('h')
+    weird_stack.print_stacks()
+
+    # pop them and look how the stacks are
+    weird_stack.pop()
+    weird_stack.pop()
+    weird_stack.pop()
+    weird_stack.print_stacks()
+
+    # insert more items 
+    weird_stack.push('i')
+    weird_stack.push('j')
+    weird_stack.push('k')
+    weird_stack.print_stacks()
 
 
 if __name__ == '__main__':
