7b1555b13b2b127aabf493f306b4a728c82d7051
larissalages <larissalages7@gmail.com>
2021-06-26 19:45:06 -0300

    still contains weird bugs

--- cracking_the_code/chapter4/graph.py
+++ cracking_the_code/chapter4/graph.py
@@ -3,42 +3,42 @@ class Node:
         self.value = value
         self.neighbors = neighbors
 
-    def add_neighbor(self, neighbor_v, weight):
-        self.neighbors[neighbor_v] = weight
+    def add_neighbor(self, neighbor, weight=0):
+        self.neighbors[neighbor] = weight
+
+    def get_connections(self):
+        l = []
+        for n in self.neighbors:
+            l.append(n.value)
+        return l
 
 
 class Graph:
     def __init__(self):
-        self.vertices = []
+        self.vertices = {}
 
     def get_vertex(self, value):
-        for v in self.vertices:
-            if v.value == value:
-                return v
-
-    def vertex_already_exist(self, vtx):
-        find_vertex = [True for x in self.vertices if x.value == vtx]
-        if any(find_vertex) is False:
-            return False
+        if value in self.vertices:
+            return self.vertices[value]
         else:
-            return True
+            return None
 
     def add_vertex(self, value):
         n = Node(value)
-        self.vertices.append(n)
+        self.vertices[value] = n
         return n
 
     def add_edge(self, vtx1, vtx2, weight):
-        node1 = self.get_vertex(vtx1)
-        if node1 is None:
-            node1 = self.add_vertex(vtx1)
-        node2 = self.get_vertex(vtx2)
-        if node2 is None:
-            node2 = self.add_vertex(vtx2)
+        if vtx1 not in self.vertices:
+            vtx1 = self.add_vertex(vtx1)
+        if vtx2 not in self.vertices:
+            vtx2 = self.add_vertex(vtx2)
+        self.vertices[vtx1].add_neighbor(self.vertices[vtx2], weight)
+        self.vertices[vtx2].add_neighbor(self.vertices[vtx1], weight)
 
-        node1.add_neighbor(vtx2, weight)
-        node2.add_neighbor(vtx1, weight)
+    def get_vertices(self):
+        return self.vertices.keys()
 
     def print(self):
         for v in self.vertices:
-            print('{}: {}'.format(v.value, v.neighbors))
+            print('{}: {}'.format(v, self.vertices[v].get_connections()))
