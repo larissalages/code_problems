a8dc7795d048978f74721f8442fb3629ee0d8c9e
larissalages <larissalages7@gmail.com>
2021-07-08 21:31:07 -0300

    improving solution

--- leetcode/python/sorting_and_searching/merge_sorted_array.py
+++ leetcode/python/sorting_and_searching/merge_sorted_array.py
@@ -3,15 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        while m > 0 and n > 0:
-            if nums1[m-1] >= nums2[n-1]:
+        while n:
+            if m and nums1[m-1] >= nums2[n-1]:
                 nums1[m+n-1] = nums1[m-1]
                 m -= 1
             else:
                 nums1[m+n-1] = nums2[n-1]
                 n -= 1
-        while n > 0:
-            nums1[:n] = nums2[:n]
 
 
 def main():
