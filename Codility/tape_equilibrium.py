7051e87c7e4dc3c5f596c76b5e9b1ae73f4910a2
larissalages <llo@cin.ufpe.br>
2021-09-16 22:22:35 -0300

    add right solution

--- Codility/tape_equilibrium.py
+++ Codility/tape_equilibrium.py
@@ -3,9 +3,10 @@ def solution(A):
     m = float('inf')
     left_sum = 0
 
-    for n in A:
+    for n in A[:-1]:
         left_sum += n
-        v = abs(total - left_sum)
+        v = abs(total - 2*left_sum)
         if v < m:
             m = v
     return m
+
