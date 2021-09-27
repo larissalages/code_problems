2bd0f7b9af39265cef1a288f47354ea58545cc1c
larissalages <llo@cin.ufpe.br>
2021-09-26 22:54:18 -0400

    optimal solution for amxCounters

--- Codility/maxCounters.py
+++ Codility/maxCounters.py
@@ -1,13 +1,19 @@
 def solution(N, A):
     counters = [0]*(N+1)
-    max_ = 0
+    max_counter = 0
+    max_current = 0
     for i in A:
-        if i == N+1:
-            for j in range(1, len(counters)):
-                counters[j] = max_
+        if 1 <= i <= N:
+            if max_counter > counters[i-1]:
+                counters[i-1] = max_counter
+            counters[i-1] += 1
+            if counters[i-1] > max_current:
+                max_current = counters[i-1]
         else:
-            counters[i] += 1
-            if counters[i] > max_:
-                max_ = counters[i]
-    del counters[0]
+            max_counter = max_current
+
+    for i in range(len(counters)):
+        if max_counter > counters[i]:
+             counters[i] = max_counter
+
     return counters
