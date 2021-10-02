ddd649819306baae7f7b446e963e9e78eb304439
larissalages <llo@cin.ufpe.br>
2021-10-02 15:51:46 -0400

    add optimal code for missing integer

--- Codility/missing_integer.py
+++ Codility/missing_integer.py
@@ -1,11 +1,9 @@
 def solution(A):
-    if len(A) == 1:
-        return A[0] + 1
-    A = sorted(A)
-    A = [num if num > 0 else 0 for num in A]
-    for i in range(len(A)):
-        if i+1<len(A) and A[i] > 0:
-            if A[i+1] != A[i]+1 and A[i+1] != A[i]:
-                return A[i] +1
+	dict_ = {}
+	for elem in A:
+		dict_[elem] = 1
+	for i in range(1, len(A)+1):
+		if not dict_.get(i):
+			return i
+	return len(A) + 1
 
-    return A[-1] + 1
