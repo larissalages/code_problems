53b618f6d0c091e496d035c072b04022bd0de6cd
sakshi-kst <sakshi.kst@gmail.com>
2021-10-09 23:10:06 +0530

    Hackerrank- Java 30DaysOfCode solutions for day 17, 19 and 20 added

---
+++ Hackerrank/java/30 days of code/Day19_Interfaces.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic {
+   int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) 
+    {
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+                sum += i;
+        }
+        return sum;
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
