8bd1b6188da71d8f744c3c2e30203ec40f02dfed
Baruni Priya T S <68952987+barunipriyats@users.noreply.github.com>
2021-10-24 06:09:58 +0530

    Create Day7_Arrays.java

---
+++ Hackerrank/java/30 days of code/Day7_Arrays.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        for(int j = n-1; j>=0; j--) {
+            System.out.print(arr[j]+" ");
+        }
+
+        scanner.close();
+    }
+}
