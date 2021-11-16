83db5e70c1f213a58a55f503fa69a032c87dd2c7
larissalages <llo@cin.ufpe.br>
2021-11-15 23:36:10 -0500

    leetcode consecutive-numbers problem

---
+++ leetcode/sql/consecutive_numbers.sql
@@ -0,0 +1,18 @@
+SELECT DISTINCT L1.num as ConsecutiveNums
+FROM
+Logs L1,
+Logs L2,
+Logs L3
+WHERE 
+    L1.id = L2.id -1
+AND L2.id = L3.id -1
+AND L1.num = L2.num
+AND L3.num = L2.num;
+
+# OR
+
+SELECT DISTINCT num as ConsecutiveNums
+FROM Logs
+WHERE (Id+1, num) in (select * from Logs) and (Id+2, num) in (select * from Logs);
+
+# Not really two much time difference between the two queries
