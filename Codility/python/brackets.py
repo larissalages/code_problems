c20e7a28c6b404274e693accf3f8fc8f51e9784e
larissalages <llo@cin.ufpe.br>
2022-01-30 20:24:17 -0500

    fix code

--- Codility/python/brackets.py
+++ Codility/python/brackets.py
@@ -9,7 +9,10 @@ def solution(S):
         if s in d.keys(): # list of open characters 
             stack.append(s)
         elif s in close_list:
-            value = stack.pop()
-            if s != d.get(value):
+            if len(stack) > 0:
+                value = stack.pop()
+                if s != d.get(value):
+                    return 0
+            else:
                 return 0
-    return 1
+    return 1 if len(stack) == 0 else 0
